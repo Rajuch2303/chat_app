c8bc4e647b2f0dedbf0d994122b8b7dcd214a73d
Raju <rkc4240@gmail.com>
2026-02-22 19:35:50 +0530

    allow action cable origins

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,15 +1,11 @@
-import consumer from "channels/consumer"
+import consumer from "./consumer"
 
 consumer.subscriptions.create("ChatroomChannel", {
   connected() {
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected() {
-    // Called when the subscription has been terminated by the server
+    console.log("✅ Connected to ChatroomChannel")
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    console.log("📩", data)
   }
-});
+})
